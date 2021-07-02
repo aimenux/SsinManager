cdff9017159cf049dd9f9aa13ed00ac3e79793fe
Aymen TROUDI <Aymen.TROUDI@sodexo.com>
2021-07-02 02:17:48 +0200

    Add ci.yml

--- Tests/Attributes/CountryValidatorAttributeTests.cs
+++ Tests/Attributes/CountryValidatorAttributeTests.cs
@@ -8,32 +8,32 @@ namespace Tests.Attributes
     public class CountryValidatorAttributeTests
     {
         [Theory]
-        [InlineData("BE")]
-        [InlineData("nl-BE")]
-        public void Should_Be_Valid_Culture(string culture)
+        [InlineData("Belgium")]
+        [InlineData("BELGIUM")]
+        public void Should_Be_Valid_Country(string country)
         {
             // arrange
             var validator = new CountryValidatorAttribute();
-            var context = new ValidationContext(culture);
+            var context = new ValidationContext(country);
 
             // act
-            var result = validator.GetValidationResult(culture, context);
+            var result = validator.GetValidationResult(country, context);
 
             // assert
             result.Should().Be(ValidationResult.Success);
         }
 
         [Theory]
-        [InlineData("fr-FR")]
-        [InlineData("es-ES")]
-        public void Should_not_Be_Valid_Culture(string culture)
+        [InlineData("Spain")]
+        [InlineData("France")]
+        public void Should_not_Be_Valid_Country(string country)
         {
             // arrange
             var validator = new CountryValidatorAttribute();
-            var context = new ValidationContext(culture);
+            var context = new ValidationContext(country);
 
             // act
-            var result = validator.GetValidationResult(culture, context);
+            var result = validator.GetValidationResult(country, context);
 
             // assert
             result.Should().NotBe(ValidationResult.Success);
